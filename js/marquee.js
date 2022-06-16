62310a0955822117df1a61026de3b459632b215e
RybkaJakub <jakubekrybka@gmail.com>
2022-06-16 18:58:20 +0200

    Version 2.0

--- js/marquee.js
+++ js/marquee.js
@@ -1,5 +1,11 @@
 const toggleBtn = document.querySelector('#toggleBtn');
 const toggleBtn1 = document.querySelector('#toggleBtn1');
+const toggleBtn2 = document.querySelector('#toggleBtn2');
+const toggleBtn3 = document.querySelector('#toggleBtn3');
+const toggleBtn4 = document.querySelector('#toggleBtn4');
+const toggleBtn5 = document.querySelector('#toggleBtn5');
+const toggleBtn6 = document.querySelector('#toggleBtn6');
+const toggleBtn7 = document.querySelector('#toggleBtn7');
 const divlist = document.querySelector('.marquee');
 divlist.style.display = 'none';
 toggleBtn.addEventListener('click', () => {
@@ -16,4 +22,53 @@ toggleBtn1.addEventListener('click', () => {
     } else {
         divlist.style.display = 'none';
     }
+
+});
+toggleBtn2.addEventListener('click', () => {
+    if (divlist.style.display == 'none') {
+        divlist.style.display = 'block';
+    } else {
+        divlist.style.display = 'none';
+    }
+
+});
+toggleBtn3.addEventListener('click', () => {
+    if (divlist.style.display == 'none') {
+        divlist.style.display = 'block';
+    } else {
+        divlist.style.display = 'none';
+    }
+
+});
+toggleBtn4.addEventListener('click', () => {
+    if (divlist.style.display == 'none') {
+        divlist.style.display = 'block';
+    } else {
+        divlist.style.display = 'none';
+    }
+
+});
+toggleBtn5.addEventListener('click', () => {
+    if (divlist.style.display == 'none') {
+        divlist.style.display = 'block';
+    } else {
+        divlist.style.display = 'none';
+    }
+
+});
+toggleBtn6.addEventListener('click', () => {
+    if (divlist.style.display == 'none') {
+        divlist.style.display = 'block';
+    } else {
+        divlist.style.display = 'none';
+    }
+
+});
+toggleBtn7.addEventListener('click', () => {
+    if (divlist.style.display == 'none') {
+        divlist.style.display = 'block';
+    } else {
+        divlist.style.display = 'none';
+    }
+
 });
